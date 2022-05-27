5cc48f0380d15322c423df3b8275b19545aca840
siliconninja <siliconninja@users.noreply.github.com>
2022-05-27 02:11:24 +0000

    Create alpha of Linux version using MonoDevelop/GTK#2.0

---
+++ RhythmJumper-Linux/RhythmJumper-Linux/OkDialogWithCloseExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+
+namespace RhythmJumperLinux
+{
+	public class OkDialogWithCloseExtension
+	{
+		private MessageDialog d;
+
+		public OkDialogWithCloseExtension (Window parentWindow, String text)
+		{
+			// https://stackoverflow.com/a/4432573
+			d = new MessageDialog (parentWindow, DialogFlags.DestroyWithParent | DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, 
+				text);
+		}
+
+		public void show() {
+			try {
+				d.Run ();
+			}
+			finally {
+				if (d != null)
+					d.Destroy ();
+			}
+		}
+	}
+}
+
